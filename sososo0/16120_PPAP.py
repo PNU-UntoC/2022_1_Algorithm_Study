1ee6275149795207982b539784234eafe3d1f126
sososo0 <94467302+sososo0@users.noreply.github.com>
2022-05-15 01:53:47 +0900

    [BOJ] PPAP/골드4/3분
    https://www.acmicpc.net/problem/16120
    
    stack, 구현

---
+++ sososo0/16120_PPAP.py
@@ -0,0 +1,13 @@
+word = input()
+stack = []
+ppap = ["P", "P", "A", "P"]
+for i in range(len(word)):
+    stack.append(word[i])
+    if stack[-4:] == ppap:
+        for _ in range(4):
+            stack.pop()
+        stack.append("P")
+if stack == ppap or stack == ["P"]:
+    print("PPAP")
+else:
+    print("NP")
